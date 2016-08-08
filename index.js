03d7b45d1b3298ba4d24df6a75b27fbce6b5c9ad
yuwei.byw <yuwei@alipay.com>
2016-08-08 21:39:32 +0800

    plugin name

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ module.exports = {
 	hooks: {
 		"page:before": function (page) {
       var md = 6;
-      if (this.options.pluginsConfig.toc.maxDepth) {
-        md = this.options.pluginsConfig.toc.maxDepth;
+      if (this.options.pluginsConfig.toc3.maxDepth) {
+        md = this.options.pluginsConfig.toc3.maxDepth;
       }
 			page.content = toc.insert(page.content, {
 				slugify: function (str) {
@@ -14,8 +14,8 @@ module.exports = {
         },
         maxdepth: md,
 			});
-			if (this.options.pluginsConfig.toc.addClass) {
-				var className = this.options.pluginsConfig.toc.className || 'toc';
+			if (this.options.pluginsConfig.toc3.addClass) {
+				var className = this.options.pluginsConfig.toc3.className || 'toc';
 				page.content = page.content + '\n\n\n<script type="text/javascript">var targetUl = document.getElementsByClassName(\'page-inner\')[0].getElementsByTagName(\'ul\')[0];if(targetUl.getElementsByTagName(\'a\').length>0){targetUl.className=\'' + className + '\';}</script>';
 			}
 			return page;
